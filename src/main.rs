b73fbdd91ff679e549fb831055e73c838f755624
Titouan Fuchs <developper@titouanfuchs.fr>
2024-03-13 19:42:19 +0100

    déclarations de variables numériques

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let test_var = 50; //Déclaration var typées automatiquement
+    let test_var_typee: u8 = 52;
+    
+    println!("Print de la variable : {test_var}");
 }
