d27bd3104949f54e8239ae3ceefa541dd42fb8fa
Titouan Fuchs <developper@titouanfuchs.fr>
2024-03-14 21:56:29 +0100

    fonction typage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 fn main() {
+    typage();
+}
+
+fn typage(){
     let test_var = 50; //Déclaration var typées automatiquement
     let test_var_typee: u8 = 52;
-    
+    let test_var_typee_diff = 52u8;
+    let bigvar:i64  = 15_000_000_000;
+    let hex = 0xff;
+    let bin = 0b10011;
+
+
     println!("Print de la variable : {test_var}");
+    println!("Print de la variable : {test_var_typee}");
+    println!("Print de la variable : {test_var_typee_diff}");
+    println!("Print de la variable : {bigvar}");
+    println!("Print de la variable : {hex}");
+    println!("Print de la variable : {bin}");
 }
