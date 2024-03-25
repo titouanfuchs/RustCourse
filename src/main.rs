ebb476b8ec72703e5be9a97ceb4bdc90c01c6058
Titouan Fuchs <developper@titouanfuchs.fr>
2024-03-25 13:58:43 +0100

    retour if, match, patternBinding etc

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,53 @@ fn main() {
     typage();
     mutabilite();
     ownership();
+    returnFromIf();
+    matchSwitch();
+    patternBinding();
+    matchPatternBinding();
+}
+
+fn matchPatternBinding(){
+    let a = ("div", 50, 3);
+
+    let (op, res) = match a { 
+        (op, x, y) if op == "div" => ("Division", x/y),
+        _ => ("erreur", 0)
+    };
+    
+    println!("Opération : {}, Résultat : {}", op, res);
+}
+
+fn patternBinding(){
+    let (a, b) = (13, 14);
+    
+    println!("{a} - {b}");
+    
+    let (c, _, d) = (15, 16, 17);
+    
+    println!("{c} - {d}");
+}
+
+fn matchSwitch(){
+    let a: i32 = 8;
+    
+    match a {
+        n if n < 0 => println!("erreur {n}"),
+        _ if a == 6 => println!("6"),
+        2 | 4 | 8 => println!("2 4 ou 8 !"),
+        0 => println!("zéro"),
+        1..=17 => println!("mineur"),
+        _=> println!("majeur")
+    }
+}
+
+fn returnFromIf(){
+    let a = 13;
+    let b = 15;
+
+    let result: String = if a < b { "true".to_owned() } else { "false".to_owned() } ;
+
+    println!("{result}");
 }
 
 fn ownership(){
