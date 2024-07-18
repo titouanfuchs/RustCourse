7bc19260986af75664e87bfaa1da151c6076aecf
Titouan Fuchs <developper@titouanfuchs.fr>
2024-07-18 16:18:51 +0200

    functions, collections, modules

---
+++ src/hello_module/mod.rs
@@ -0,0 +1,5 @@
+pub mod hello_mod {
+    pub fn hello() {
+        println!("Hello Module");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rand::Rng;
+use std::collections::HashMap;
+
+mod hello_module;
+use crate::hello_module::hello_mod;
+
 fn main() {
     typage();
     mutabilite();
@@ -8,84 +14,129 @@ fn main() {
     matchPatternBinding();
     arrays();
     tuples();
+    vectors();
+    hashMaps();
+
+    match get_random_number(-10, 10) {
+        None => println!("None"),
+        Some(x) => println!("Random number : {}", x),
+    }
+
+    let text: String = String::from("value");
+    say(&text);
+    say(&text);
+
+    hello_mod::hello();
 }
 
-fn tuples(){
+fn say(text: &String) {
+    println!("{}", text);
+}
+
+fn get_random_number(min: i32, max: i32) -> Option<i32> {
+    if (min > max || max < min) {
+        return None;
+    }
+
+    let mut rng = rand::thread_rng();
+
+    return Some(rng.gen_range(min..max));
+}
+
+fn hashMaps() {
+    let mut a = HashMap::new();
+
+    a.insert("test", 13u8);
+
+    println!("{:?}", a);
+}
+
+fn vectors() {
+    let mut vec = vec![1, 3, 4, 5];
+
+    println!("{:?}", vec);
+}
+
+fn tuples() {
     let a = (1, 2, 3, "string");
     let b = (1, (2, (3, 4)));
-    
+
     println!("{}", a.3);
-    println!("{}", b.1.1.0);
+    println!("{}", b.1 .1 .0);
 }
 
-fn arrays(){
+fn arrays() {
     let mut a: [u8; 4] = [1, 2, 3, 4];
     println!("{:?}", a);
-    
-    a[2] = 16;    
+
+    a[2] = 16;
     println!("{:?}", a);
-    
+
     let slice = &mut a[0..3];
-    
+
     println!("Slice: {:?}", slice);
     slice[1] = 67;
-    
+
     println!("Array : {:?}", a);
 }
 
-fn matchPatternBinding(){
+fn matchPatternBinding() {
     let a = ("div", 50, 3);
 
-    let (op, res) = match a { 
-        (op, x, y) if op == "div" => ("Division", x/y),
-        _ => ("erreur", 0)
+    let (op, res) = match a {
+        (op, x, y) if op == "div" => ("Division", x / y),
+        _ => ("erreur", 0),
     };
-    
+
     println!("Opération : {}, Résultat : {}", op, res);
 }
 
-fn patternBinding(){
+fn patternBinding() {
     let (a, b) = (13, 14);
-    
+
     println!("{a} - {b}");
-    
+
     let (c, _, d) = (15, 16, 17);
-    
+
     println!("{c} - {d}");
 }
 
-fn matchSwitch(){
+fn matchSwitch() {
     let a: i32 = 8;
-    
+
     match a {
         n if n < 0 => println!("erreur {n}"),
         _ if a == 6 => println!("6"),
         2 | 4 | 8 => println!("2 4 ou 8 !"),
         0 => println!("zéro"),
         1..=17 => println!("mineur"),
-        _=> println!("majeur")
+        _ => println!("majeur"),
     }
 }
 
-fn returnFromIf(){
+fn returnFromIf() {
     let a = 13;
     let b = 15;
 
-    let result: String = if a < b { "true".to_owned() } else { "false".to_owned() } ;
+    let result: String = if a < b {
+        "true".to_owned()
+    } else {
+        "false".to_owned()
+    };
 
     println!("{result}");
 }
 
-fn ownership(){
+fn ownership() {
     let a = "Hello".to_owned();
     let mut b = a.clone();
-    
+
     b.push_str(" World");
-    
+
     println!("{a} - {b}");
 }
 
-fn mutabilite(){
+fn mutabilite() {
     let mut a: u16 = 56;
     println!("{a}");
 
@@ -94,14 +145,13 @@ fn mutabilite(){
     println!("{a}");
 }
 
-fn typage(){
+fn typage() {
     let test_var = 50; //Déclaration var typées automatiquement
     let test_var_typee: u8 = 52;
     let test_var_typee_diff = 52u8;
-    let bigvar:i64  = 15_000_000_000;
+    let bigvar: i64 = 15_000_000_000;
     let hex = 0xff;
     let bin = 0b10011;
-
 
     println!("Print de la variable : {test_var}");
     println!("Print de la variable : {test_var_typee}");
